1beb2661cb7f9b11edaa322b5f946f177563cb05
alannanoguchi <alanna.noguchi@students.makeschool.com>
2019-09-01 16:57:26 -0700

    Added user input into the mad libs story

--- mad_libs.py
+++ mad_libs.py
@@ -13,19 +13,39 @@ def create(item):
     wordBlanks.append(item)
 
 # User fills in the blanks
-def insert_word(item):
-    insert_word = word(item)
-    return insert_word
+def insert_words(item):
+    insert_words = input(item)
+    return insert_words
 
 # Mad Libs:
 def mad_libs():
     # This is the beginning of Mad libs:
     print("Welcome.")
 
-    # User begins inserting parts of speech / Store user's inputs
+    # User is asked to enter in part of speech:
+    noun1 = insert_words("Enter a noun: ")
+    # Append the word into the list of word word blanks:
+    create(noun1)
 
+    adjective1 = insert_words("Enter adjective: ")
+    create(adjective1)
+
+    verb1 = insert_words("Enter verb: ")
+    create(verb1)
+
+    adjective2 = insert_words("Enter another adjective: ")
+    create(adjective2)
+
+    noun2 = insert_words("Enter another noun: ")
+    create(noun2)
 
     # The story with the Users inserted words:
+    print("I have a " + noun1 + " in my pocket.")
+    print("It feels " + adjective1 + ".")
+    print("You can touch it, feels it and " + verb1 + " it.")
+    print(" When it is happy, it smells " + adjective2 + ".")
+    print("It is best friends with a " + noun2 + ".")
+    
 
 # Run the Function:
 mad_libs()
