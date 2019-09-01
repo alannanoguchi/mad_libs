d37bd0528ec2e37faaf3a1b7d22aeb9c16f3542a
alannanoguchi <alanna.noguchi@students.makeschool.com>
2019-09-01 16:19:42 -0700

    New functions added

--- mad_libs.py
+++ mad_libs.py
@@ -1,5 +1,31 @@
-# Sentences
+# The Story:
 
 # I have a __(noun)__ in my pocket.
 # It feels __(adj)__. You can touch it, feed it and __(verb)__ it. When it is happy, it smells __(adj)__.
 # It is best friends with a __(noun)__.
+
+# A list of blank words:
+wordBlanks = list()
+
+# Create:
+def create(item):
+    # Adding items into the list:
+    wordBlanks.append(item)
+
+# User fills in the blanks
+def insert_word(item):
+    insert_word = word(item)
+    return insert_word
+
+# Mad Libs:
+def mad_libs():
+    # This is the beginning of Mad libs:
+    print("Welcome.")
+
+    # User begins inserting parts of speech / Store user's inputs
+
+
+    # The story with the Users inserted words:
+
+# Run the Function:
+mad_libs()
