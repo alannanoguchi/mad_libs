49a578b956b35ea0624fa050b03c75c2780cdce9
alannanoguchi <alanna.noguchi@students.makeschool.com>
2019-09-01 17:15:52 -0700

    More formatting changes

--- mad_libs.py
+++ mad_libs.py
@@ -1,7 +1,9 @@
 # The Story:
 
 # I have a __(noun)__ in my pocket.
-# It feels __(adj)__. You can touch it, feed it and __(verb)__ it. When it is happy, it smells __(adj)__.
+# It feels __(adj)__.
+# You can touch it, feed it and __(verb)__ it. 
+# When it is happy, it smells __(adj)__.
 # It is best friends with a __(noun)__.
 
 # A list of blank words:
